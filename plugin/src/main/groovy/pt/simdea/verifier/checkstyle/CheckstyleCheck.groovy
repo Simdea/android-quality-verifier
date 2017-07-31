041239926e89a1b17c2910075fd052d60cb9bb99
André Rosa <andre.rosa@simdea.pt>
2017-07-31 10:29:29 +0100

    fixed No such property: type for class: java.util.Formatter error on checkstyle

--- plugin/src/main/groovy/pt/simdea/verifier/checkstyle/CheckstyleCheck.groovy
+++ plugin/src/main/groovy/pt/simdea/verifier/checkstyle/CheckstyleCheck.groovy
@@ -1,6 +1,7 @@
 package pt.simdea.verifier.checkstyle
 
 import com.puppycrawl.tools.checkstyle.ant.CheckstyleAntTask
+import com.puppycrawl.tools.checkstyle.ant.CheckstyleAntTask.Formatter
 import com.puppycrawl.tools.checkstyle.ant.CheckstyleAntTask.FormatterType
 import groovy.util.slurpersupport.GPathResult
 import org.gradle.api.Project
