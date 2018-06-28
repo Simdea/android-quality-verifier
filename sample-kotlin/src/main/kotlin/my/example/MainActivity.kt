b458dc09799db23b4f65e5c02276af15538e20de
andrer757 <andre.rosa@simdea.pt>
2018-06-28 11:55:01 +0100

    [new] Added kotlin sample

---
+++ sample-kotlin/src/main/kotlin/my/example/MainActivity.kt
@@ -0,0 +1,20 @@
+package my.example
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    // CR Code Review
+
+    // TODO To Do
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.)
+        //LOGGER.info("MainActivity::onCreate");
+        //LOGGER.info("AndroidLibrary.process: {}", AndroidLibrary.process(this));
+        //LOGGER.info("JavaLibrary.process: {}", JavaLibrary.process(this));
+    }
+
+
+}
